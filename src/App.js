e71ada7ef7b02bd95011e09fdd85e69ea074c495
Chris Boylen <billpolster@gmail.com>
2018-10-15 16:21:55 -0600

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_DATABASE_API_URL + '/')
+      .then(response => response.json())
+      .then(data => this.setState({ data }))
+      .catch(error => console.log(error.message));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.data}
       </div>
     );
   }
