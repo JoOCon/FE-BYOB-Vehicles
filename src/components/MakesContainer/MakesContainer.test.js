8dc259b8274c5b9774669b48a7ebf7f3103d1245
Chris Boylen <billpolster@gmail.com>
2018-10-16 13:30:26 -0600

    Refactor MakesContainer snapshot test

--- src/components/MakesContainer/MakesContainer.test.js
+++ src/components/MakesContainer/MakesContainer.test.js
@@ -3,8 +3,14 @@ import { shallow } from 'enzyme';
 import MakesContainer from './MakesContainer';
 
 describe('MakesContainer', () => {
-  it('should match snapshot', () => {
-    const wrapper = shallow(<MakesContainer />);
+  it('should match snapshot with makes', () => {
+    const mockMakes = [{ name: 'garry' }, { name: 'billy' }];
+    const wrapper = shallow(
+      <MakesContainer
+        makes={mockMakes}
+      />,
+    );
+
     expect(wrapper).toMatchSnapshot();
   });
 });
