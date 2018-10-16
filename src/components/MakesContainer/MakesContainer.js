aa8e42a3f5a8df5d0438e25f400db1998b0dcb49
Chris Boylen <billpolster@gmail.com>
2018-10-16 14:04:12 -0600

    Refactor MakesCont as functional component

--- src/components/MakesContainer/MakesContainer.js
+++ src/components/MakesContainer/MakesContainer.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MakeCard from '../MakeCard/MakeCard';
 import './MakesContainer.css';
 
-class MakesContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      makes: [],
-    };
-  }
-
-  render() {
-    const { makes } = this.state;
-    const makesList = makes.map(make => (
-      <MakeCard
-        {...make}
-        key={make.id}
-      />
-    ));
-    return (
-      <section className="makes-cont">
-        {makesList}
-      </section>
-    );
-  }
-}
+const MakesContainer = (props) => {
+  const { makes } = props;
+  const makesList = makes.map(make => (
+    <MakeCard
+      {...make}
+      key={make.id}
+    />
+  ));
+  return (
+    <section className="makes-cont">
+      {makesList}
+    </section>
+  );
+};
 
 export default MakesContainer;
