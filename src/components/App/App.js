7b50ec6a47a1c5306df92a8ca28b728fbb1c4e20
Chris Boylen <billpolster@gmail.com>
2018-10-16 14:53:50 -0600

    Update fetch path in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
+import MakesContainer from '../MakesContainer/MakesContainer';
 import './App.css';
 
 class App extends Component {
@@ -11,18 +12,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch(`${process.env.REACT_APP_DATABASE_API_URL}/`)
+    fetch(`${process.env.REACT_APP_DATABASE_API_URL}/api/v1/makes`)
       .then(response => response.json())
       .then(data => this.setState({ data }))
       .catch(error => console.log(error.message));
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div className="App">
         <header>
           <SearchForm />
         </header>
+        <MakesContainer data={data} />
       </div>
     );
   }
