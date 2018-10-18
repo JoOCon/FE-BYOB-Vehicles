cd5a5d89c0eda07c866cc325b88a7d8de1f98981
Chris Boylen <billpolster@gmail.com>
2018-10-17 21:02:13 -0600

    Add rule to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         "react/jsx-filename-extension": "off",
-        "react/jsx-one-expression-per-line": "off"
+        "react/jsx-one-expression-per-line": "off",
+        "react/require-default-props": "off"
     },
     "env": {
         "jest": true
