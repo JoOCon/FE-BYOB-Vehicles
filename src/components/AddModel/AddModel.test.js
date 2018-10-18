9751112d97f5eed9770c044aae14aea6441d37c4
Chris Boylen <billpolster@gmail.com>
2018-10-17 21:39:49 -0600

    Add and pass test handleChange method should set state when invoked

--- src/components/AddModel/AddModel.test.js
+++ src/components/AddModel/AddModel.test.js
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 import AddModel from './AddModel';
 
 describe('AddModel', () => {
-  it('should match snapshot with models', () => {
-    const wrapper = shallow(<AddModel />);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<AddModel />);
+  });
+  it('should match snapshot with models', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleChange', () => {
+    it('should set state when invoked', () => {
+      const mockEvent = { target: { name: 'modelName', value: 'FF' } };
+      wrapper.instance().handleChange(mockEvent);
+
+      expect(wrapper.state().modelName).toBe('FF');
+    });
   });
 });
